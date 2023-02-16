9d938f9d2a09b3d4619c42c75d23026287021d30
Zidane Innis <zidaneinnis@gmail.com>
2023-02-16 16:50:26 -0500

    Wrapping the Transition Component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,39 @@
 import React from "react";
+import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
 const modal = (props) => {
-  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+  const cssClasses = [
+    "Modal",
+    props.show === "entering"
+      ? "ModalOpen"
+      : props.show === "exiting"
+      ? "ModalClosed"
+      : null,
+  ];
 
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition in={props.show} timeout={300} mountOnEnter mountOnExit>
+      {(state) => {
+        cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
